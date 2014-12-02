54e7143a7d72295219fee999ac33cf6139cc99c0
patmurphy <patmurphy@192.168.1.104>
2014-12-02 10:21:24 +0000

    alter config file

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,43 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table activity (
+  id                        bigint not null,
+  type                      varchar(255),
+  location                  varchar(255),
+  distance                  double,
+  constraint pk_activity primary key (id))
+;
+
+create table my_user (
+  id                        bigint not null,
+  firstname                 varchar(255),
+  lastname                  varchar(255),
+  email                     varchar(255),
+  password                  varchar(255),
+  constraint pk_my_user primary key (id))
+;
+
+create sequence activity_seq;
+
+create sequence my_user_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists activity;
+
+drop table if exists my_user;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists activity_seq;
+
+drop sequence if exists my_user_seq;
+
